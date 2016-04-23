9b4a0c9b6caad150db5d43cb97c723d9a465f675
Richard H. Olsen <einetunet@gmail.com>
2016-04-23 22:25:00 +0200

    Better fish info view

--- www/js/fishinfoCtrl.js
+++ www/js/fishinfoCtrl.js
@@ -4,5 +4,18 @@ angular.module('starter.controllers')
   var id = stateParams.id
 
    scope.info = fillets.getDetail(id);
-  
+
+
+   scope.consumptionRecommendation = function(info){
+     console.log(info);
+     if(info.recommendation ==='Neutral'){
+       return "img/recommandation/1.png";
+     }else if(info.recommendation === 'Recommended'){
+       return "img/recommandation/2.png";
+     }else if(info.recommendation === 'Not recommended'){
+       return "img/recommandation/0.png";
+     }
+
+   }
+
 }])
